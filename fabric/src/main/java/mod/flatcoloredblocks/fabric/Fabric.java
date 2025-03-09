a94324a2a64f8ce97afb6ba0b594d7350b08a75f
Marc Hermans <marc.hermans@ldtteam.com>
2025-03-09 21:46:10 +0100

    Fix logger references in init classes on Fabric.

--- fabric/src/main/java/mod/flatcoloredblocks/fabric/Fabric.java
+++ fabric/src/main/java/mod/flatcoloredblocks/fabric/Fabric.java
@@ -1,5 +1,6 @@
 package mod.flatcoloredblocks.fabric;
 
+import com.mojang.logging.LogUtils;
 import mod.flatcoloredblocks.core.FlatColoredBlocks;
 import com.communi.suggestu.scena.core.init.PlatformInitializationHandler;
 import mod.flatcoloredblocks.core.client.FlatColoredBlocksClient;
@@ -11,13 +12,13 @@ import org.slf4j.LoggerFactory;
 
 public class Fabric implements ModInitializer {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger("formula-fabric");
+    private static final Logger LOGGER = LogUtils.getLogger();
 
     private FlatColoredBlocks flatColoredBlocks;
 
     public Fabric()
     {
-        LOGGER.info("Initialized formula-forge");
+        LOGGER.info("Initialized Flat Colored Blocks");
         PlatformInitializationHandler.getInstance().onInit(platform -> setFlatColoredBlocks(new FlatColoredBlocks()));
     }
 
--- fabric/src/main/java/mod/flatcoloredblocks/fabric/client/FabricClient.java
+++ fabric/src/main/java/mod/flatcoloredblocks/fabric/client/FabricClient.java
@@ -1,16 +1,20 @@
 package mod.flatcoloredblocks.fabric.client;
 
 import com.communi.suggestu.scena.core.init.PlatformInitializationHandler;
+import com.mojang.logging.LogUtils;
 import mod.flatcoloredblocks.core.client.FlatColoredBlocksClient;
 import mod.flatcoloredblocks.fabric.Fabric;
 import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class FabricClient implements ClientModInitializer {
 
+    private static final Logger LOGGER = LogUtils.getLogger();
     private FlatColoredBlocksClient flatColoredBlocksClient;
 
     public FabricClient() {
-        Fabric.LOGGER.info("Initialized formula-forge client");
+        LOGGER.info("Initialized Flat Colored Block client");
         PlatformInitializationHandler.getInstance().onInit(platform -> setFlatColoredBlocksClient(new FlatColoredBlocksClient()));
     }
 
